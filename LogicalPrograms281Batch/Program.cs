f0d574d15eac73369e59efd3c163602339628c93
Shrikant <sawantshrikant07@gmail.com>
2023-05-24 11:32:47 +0530

    [Shrikant] add:WelcomeMeassage

---
+++ LogicalPrograms281Batch/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Logical Programming Problem(281Batch)!"); 
+    }
+}
