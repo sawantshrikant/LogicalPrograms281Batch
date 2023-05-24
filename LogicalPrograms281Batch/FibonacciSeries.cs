b38abb492a1abd2806b6d2cf832cc75f200c85b1
Shrikant <sawantshrikant07@gmail.com>
2023-05-24 11:40:24 +0530

    [Shrikant] add:PrintFibonacciSeries

---
+++ LogicalPrograms281Batch/FibonacciSeries.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LogicalPrograms281Batch;
+public class FibonacciSeries
+{
+    public void PrintFibonacciSeries(int num)
+    {
+        int firstNum = 0, secondNum = 1, fibonacciValue;
+        Console.Write(firstNum + " " + secondNum + " ");
+        for (int i = 2; i < num; i++)
+        {
+            fibonacciValue = firstNum + secondNum;
+            Console.Write(fibonacciValue + " ");
+            firstNum = secondNum;
+            secondNum = fibonacciValue;
+        }
+        Console.WriteLine();
+    }
+}
--- LogicalPrograms281Batch/Program.cs
+++ LogicalPrograms281Batch/Program.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using LogicalPrograms281Batch;
 
 public class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Logical Programming Problem(281Batch)!"); 
+        Console.WriteLine("Welcome to Logical Programming Practice Problem!");
+        bool flag = true;
+        while (flag)
+        {
+            Console.Write("1. Fibonacci\n2. Exit\nEnter your option to execute: ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibonacciSeries fib = new FibonacciSeries();
+                    Console.Write("Enter the number for Fibonacci Series: ");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    fib.PrintFibonacciSeries(num);
+                    break;
+                case 2:
+                    flag = false;
+                    break;
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+        }
     }
 }
